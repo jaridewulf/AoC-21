3fa656711a3b5a38cd0c612c95255094942dcbbf
JariDevine <jari.de.wulf@student.howest.be>
2021-12-03 13:47:11 +0100

    Add solve of first star

--- src/bin/challenge_03/main.rs
+++ src/bin/challenge_03/main.rs
@@ -3,26 +3,49 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-fn star_one(content: Vec<String>) -> Vec<u32> {
+fn star_one(content: Vec<String>) -> i32 {
     let mut col_pos = 0;
-    let mut gamma_rate: Vec<u32>;
-    while col_pos.clone() < 12 {
-        let mut column_numbers: Vec<String>;
+    let mut gamma_rate: Vec<u32> = Vec::new();
+    let mut epsilon_rate: Vec<u32> = Vec::new();
+
+    while col_pos.clone() < 13 {
+        let mut column_numbers: Vec<String> = Vec::new();
 
         for el_pos in 0..content.len() {
-            let to_check: Vec<_> = content[el_pos].split_whitespace().collect();
+            let to_check: Vec<_> = content[el_pos].split("").collect();
             column_numbers.push(to_check[col_pos].to_owned());
         }
 
         if column_numbers.iter().filter(|&n| *n == "1").count() > column_numbers.len() / 2 {
             gamma_rate.push(1);
-        } else {
+        }
+        if column_numbers.iter().filter(|&n| *n == "0").count() > column_numbers.len() / 2 {
             gamma_rate.push(0);
         }
-
         col_pos += 1;
     }
-    return gamma_rate;
+
+    for (i, _val) in gamma_rate.iter().enumerate() {
+        if gamma_rate[i] == 0 {
+            epsilon_rate.push(1);
+        }
+        if gamma_rate[i] == 1 {
+            epsilon_rate.push(0);
+        }
+    }
+
+    let gamma_rate_string: String = gamma_rate
+        .into_iter()
+        .map(|d| std::char::from_digit(d, 10).unwrap())
+        .collect();
+
+    let epsilon_rate_string: String = epsilon_rate
+        .into_iter()
+        .map(|d| std::char::from_digit(d, 10).unwrap())
+        .collect();
+
+    return i32::from_str_radix(&gamma_rate_string, 2).unwrap()
+        * i32::from_str_radix(&epsilon_rate_string, 2).unwrap();
 }
 
 fn main() {
